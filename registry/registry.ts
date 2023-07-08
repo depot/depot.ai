c188ca4280b0acf93a828862946710eceb3d7b52
Jacob Gillespie <jacobwgillespie@gmail.com>
2023-07-08 11:02:09 +0100

    Clean up registry implementation

--- registry/registry.ts
+++ registry/registry.ts
@@ -1,37 +1,18 @@
-/* eslint-disable @typescript-eslint/no-non-null-assertion */
-/* eslint-disable @typescript-eslint/no-floating-promises */
-import type {HonoRequest} from 'hono'
 import {Hono} from 'hono'
-import {z} from 'zod'
-import {ERROR, emptyResponse, errorResponse, response} from './utils'
+import type {Env} from './utils'
+import {
+  emptyResponse,
+  getObject,
+  importBlob,
+  importManifest,
+  json,
+  parseDigest,
+  parseTag,
+  resolveTagToDigest,
+  response,
+} from './utils'
 
-function proxyArtifactRegistry(req: HonoRequest<any>, authorization: string) {
-  const url = new URL(req.url)
-  url.protocol = 'https:'
-  url.hostname = 'us-docker.pkg.dev'
-  url.pathname = `/v2/depot-gcp/depot-ai${url.pathname.replace('/v2/', '/').replace('/_slurp', '')}`
-
-  const headers = new Headers(req.headers)
-  headers.delete('range')
-  headers.set('Authorization', authorization)
-
-  return fetch(url, {method: req.method, headers})
-}
-
-interface Env {
-  Bindings: {
-    storage: R2Bucket
-    uploadSessions: DurableObjectNamespace
-    REGISTRY_TOKEN: string
-  }
-}
-
-const app = new Hono<Env>().basePath('/')
-export default app
-
-/***********************************************
- * Base
- ***********************************************/
+const app = new Hono<Env>()
 
 app.use('*', async (c, next) => {
   c.executionCtx.passThroughOnException()
@@ -47,129 +28,70 @@ app.use('*', async (c, next) => {
 app.get('/', ({text}) => text('🔮'))
 app.get('/v2/', ({text}) => text('🔮'))
 
-/***********************************************
- * Manifests
- ***********************************************/
-
 app.get('/v2/:name{.*}/manifests/:reference', async ({req, env}) => {
-  return proxyArtifactRegistry(req, `Basic ${env.REGISTRY_TOKEN}`)
-
   const name = req.param('name')
   const reference = req.param('reference')
 
-  let digest = parseDigest(reference)?.digest
+  let digest = parseDigest(reference)
   const tag = parseTag(reference)
 
   // If tag is supplied, resolve it to a digest
-  if (tag) {
-    const obj = await env.storage.get(`${name}/tags/${tag}`)
-    if (!obj) return emptyResponse({status: 404})
-    digest = await obj.text()
-  }
+  if (tag) digest = await resolveTagToDigest(env, name, reference)
+  if (!digest) return json({errors: [{code: 'MANIFEST_UNKNOWN'}]}, {status: 404})
 
-  if (digest) {
-    const headers = new Headers({'Docker-Content-Digest': digest})
+  const headers = new Headers({
+    'Docker-Content-Digest': digest.digest,
+    'Docker-Distribution-Api-Version': 'registry/2.0',
+  })
 
-    if (req.method === 'HEAD') {
-      const object = await env.storage.head(`${name}/manifests/${digest}`)
-      if (object) {
-        object.writeHttpMetadata(headers)
-        headers.set('Content-Length', object.size.toString())
-        return emptyResponse({headers})
-      }
-    } else {
-      const object = await env.storage.get(`${name}/manifests/${digest}`)
-      if (object) {
-        object.writeHttpMetadata(headers)
-        return response(object.body, {headers})
-      }
+  if (req.method === 'HEAD') {
+    let object = await env.storage.head(`${name}/manifests/${digest.digest}`)
+    if (!object) {
+      const res = await importManifest(env, name, digest)
+      if (!res.ok) return res
+      object = await env.storage.head(`${name}/manifests/${digest.digest}`)
     }
+    if (object) {
+      object.writeHttpMetadata(headers)
+      headers.set('Content-Length', object.size.toString())
+      return emptyResponse({headers})
+    }
+    return json({errors: [{code: 'MANIFEST_UNKNOWN'}]}, {status: 404})
   }
 
-  return emptyResponse({status: 404})
+  const object = await env.storage.get(`${name}/manifests/${digest.digest}`)
+  if (!object) return await importManifest(env, name, digest)
+  object.writeHttpMetadata(headers)
+  return response(object.body, {headers})
 })
-
-app.post('/v2/:name{.*}/manifests/:reference/_slurp', async ({req, env}) => {
-  const name = req.param('name')
-  const reference = req.param('reference')
-
-  const url = new URL(req.url)
-  url.hostname = 'us-docker.pkg.dev'
-  url.pathname = `/v2/depot-gcp/depot-ai${url.pathname.replace('/v2/', '/').replace('/_slurp', '')}`
-
-  console.log(`Slurping ${url}`, {
-    Authorization: `Basic ${env.REGISTRY_TOKEN}`,
-  })
-
-  const res = await fetch(url, {
-    method: 'GET',
-    headers: {
-      Authorization: `Basic ${env.REGISTRY_TOKEN}`,
-      Accept: '*/*',
-    },
-  })
-
-  console.log(res.status, res.statusText)
-
-  return res
-})
-
-/***********************************************
- * Blobs
- ***********************************************/
 
 app.get('/v2/:name{.*}/blobs/:digest', async ({req, env}) => {
   const name = req.param('name')
-  const digest = req.param('digest')
+  const digest = parseDigest(req.param('digest'))
+  if (!digest) return json({errors: [{code: 'BLOB_UNKNOWN'}]}, {status: 404})
+  const key = `${name}/blobs/${digest.digest}`
 
-  const key = `${name}/blobs/${digest}`
-  console.log(key, [...req.headers.entries()])
-
-  if (!(await env.storage.head(key))) {
-    console.log(`Attempting to slurp ${key} from artifact registry`)
-    const res = await proxyArtifactRegistry(req, `Basic ${env.REGISTRY_TOKEN}`)
-    if (!res.ok) return res
-    const object = await env.storage.put(key, res.body!, {
-      sha256: digest.replace('sha256:', ''),
-      httpMetadata: {
-        contentEncoding: res.headers.get('content-encoding') ?? undefined,
-        contentType: res.headers.get('content-type') ?? undefined,
-      },
-    })
-    console.log('success', object)
-  }
+  const headers = new Headers({
+    'Docker-Content-Digest': digest.digest,
+    'Docker-Distribution-Api-Version': 'registry/2.0',
+  })
 
   if (req.method === 'HEAD') {
-    const obj = await env.storage.head(key)
-    if (!obj) return emptyResponse({status: 404})
-    return emptyResponse({
-      headers: {
-        'Content-Length': obj.size.toString(),
-        'Docker-Content-Digest': digest,
-      },
-    })
+    let object = await env.storage.head(key)
+    if (!object) {
+      const res = await importBlob(env, name, digest)
+      if (!res.ok) return res
+      object = await env.storage.head(key)
+    }
+    if (!object) return json({errors: [{code: 'BLOB_UNKNOWN'}]}, {status: 404})
+    object.writeHttpMetadata(headers)
+    headers.set('Content-Length', object.size.toString())
+    return emptyResponse({headers})
   }
 
-  const obj = await env.storage.get(key, {onlyIf: req.headers, range: req.headers})
-  if (!obj) return errorResponse([ERROR.BLOB_UNKNOWN], {status: 404})
-  const headers = new Headers({
-    // 'Content-Length': obj.size.toString(),
-    // 'Content-Type': 'application/octet-stream',
-    'Docker-Content-Digest': digest,
-  })
-  console.log(obj.httpMetadata)
-  obj.writeHttpMetadata(headers)
-  headers.set('etag', obj.httpEtag)
-  if (obj.range) {
-    headers.set('content-range', `bytes ${obj.range.offset}-${obj.range.offset + obj.range.length - 1}/${obj.size}`)
-  }
-  headers.set('content-length', (obj.range?.length ?? obj.size).toString())
-  const status = obj.body ? (req.headers.get('range') !== null ? 206 : 200) : 304
-  console.log('Handling with R2', key, status, obj.range, JSON.stringify([...headers.entries()]))
-  return new Response(obj.body, {
-    headers,
-    status,
-  })
+  const res = await getObject(req, env, key, headers)
+  if (!res) return importBlob(env, name, digest)
+  return res
 })
 
 app.notFound(({json}) => json({errors: [{code: 'NOT_FOUND', message: 'not found'}]}, 404))
@@ -179,77 +101,4 @@ app.onError((err, {json}) => {
   return json({errors: [{code: 'INTERNAL_ERROR', message: err.message}]}, 500)
 })
 
-const tagSchema = z.string().regex(/^[\w][\w.-]{0,127}$/)
-
-export function parseTag(candidate: string) {
-  const result = tagSchema.safeParse(candidate)
-  return result.success ? result.data : null
-}
-
-export interface Digest {
-  readonly algorithm: 'sha256' | 'sha384' | 'sha512'
-  readonly hash: string
-  readonly digest: string
-}
-
-const algorithmToCrypto = {sha256: 'SHA-256', sha384: 'SHA-384', sha512: 'SHA-512'} as const
-
-const digestSchema = z.string().regex(/^[a-z0-9]+(?:[.+_-][a-z0-9]+)*:[a-zA-Z0-9=_-]+$/)
-export function parseDigest(candidate: string): Digest | null {
-  const result = digestSchema.safeParse(candidate)
-  if (!result.success) return null
-  const [algorithm, hash] = result.data.split(':')
-  if (algorithm !== 'sha256' && algorithm !== 'sha384' && algorithm !== 'sha512') return null
-  if (!hash) return null
-  return {algorithm, hash, digest: result.data}
-}
-
-function arrayBufferToHex(buffer: ArrayBuffer) {
-  return Array.from(new Uint8Array(buffer))
-    .map((b) => b.toString(16).padStart(2, '0'))
-    .join('')
-}
-
-export async function digestOfStream(algorithm: Digest['algorithm'], stream: ReadableStream) {
-  const digestStream = new crypto.DigestStream(algorithmToCrypto[algorithm])
-  stream.pipeTo(digestStream)
-  const digestBuffer = await digestStream.digest
-  return `${algorithm}:${arrayBufferToHex(digestBuffer)}`
-}
-
-export async function digestOfString(algorithm: Digest['algorithm'], string: string) {
-  const digestBuffer = await crypto.subtle.digest(algorithmToCrypto[algorithm], new TextEncoder().encode(string))
-  return `${algorithm}:${arrayBufferToHex(digestBuffer)}`
-}
-
-export async function storeWithDigest(
-  storage: R2Bucket,
-  key: string,
-  stream: ReadableStream,
-  algorithm: Digest['algorithm'],
-) {
-  const [a, b] = stream.tee()
-  const digest = await digestOfStream(algorithm, a)
-  return await storage.put(key, b, {customMetadata: {digest: digest}})
-}
-
-export const supportedManifestContentTypes = [
-  'application/vnd.docker.distribution.manifest.v2+json',
-  'application/vnd.docker.distribution.manifest.list.v2+json',
-
-  'application/vnd.oci.descriptor.v1+json',
-  'application/vnd.oci.layout.header.v1+json',
-  'application/vnd.oci.image.index.v1+json',
-  'application/vnd.oci.image.manifest.v1+json',
-  'application/vnd.oci.image.config.v1+json',
-]
-
-export const supportedLayerContentTypes = [
-  'application/vnd.docker.image.rootfs.diff.tar.gzip',
-
-  'application/vnd.oci.image.layer.v1.tar',
-  'application/vnd.oci.image.layer.v1.tar+gzip',
-  'application/vnd.oci.image.layer.v1.tar+zstd',
-]
-
-export const supportedContentTypes = [...supportedManifestContentTypes, ...supportedLayerContentTypes]
+export default app
--- registry/utils.ts
+++ registry/utils.ts
@@ -1,3 +1,14 @@
+import type {HonoRequest} from 'hono'
+import {z} from 'zod'
+
+export interface Env {
+  Bindings: {
+    storage: R2Bucket
+    REGISTRY_TOKEN: string
+    UPSTREAM_REGISTRY: string
+  }
+}
+
 export function response(bodyInit?: BodyInit | null | undefined, init: ResponseInit = {}): Response {
   const headers = new Headers(init.headers || {})
   headers.set('Docker-Distribution-API-Version', 'registry/2.0')
@@ -16,71 +27,131 @@ export function json(data: any, responseInit: ResponseInit = {}): Response {
   return response(JSON.stringify(data), responseInit)
 }
 
-export interface ResponseError {
-  code: string
-  message: string
-  detail?: any
+let upstreamURL: URL | undefined
+
+export async function importManifest(env: Env['Bindings'], name: string, digest: Digest) {
+  upstreamURL = upstreamURL ?? new URL(env.UPSTREAM_REGISTRY)
+
+  const url = new URL(`https://registry/v2/${upstreamURL.pathname}/${name}/manifests/${digest.digest}`)
+  url.protocol = upstreamURL.protocol
+  url.hostname = upstreamURL.hostname
+
+  const headers = new Headers()
+  headers.set('accept', '*/*')
+  headers.set('authorization', `Basic ${env.REGISTRY_TOKEN}`)
+
+  const res = await fetch(url, {headers})
+  if (!res.ok || !res.body) return res
+
+  await env.storage.put(`${name}/manifests/${digest.digest}`, res.body, {
+    sha256: digest.hash,
+    httpMetadata: {
+      contentEncoding: res.headers.get('content-encoding') ?? undefined,
+      contentType: res.headers.get('content-type') ?? undefined,
+    },
+  })
+
+  const obj = await env.storage.get(`${name}/manifests/${digest.digest}`)
+  if (!obj) return new Response(null, {status: 404})
+  return new Response(obj.body, res)
 }
 
-export function errorResponse(errors: ResponseError[], responseInit: ResponseInit = {status: 400}) {
-  return json({errors}, responseInit)
+export async function importBlob(env: Env['Bindings'], name: string, digest: Digest) {
+  upstreamURL = upstreamURL ?? new URL(env.UPSTREAM_REGISTRY)
+
+  const url = new URL(`https://registry/v2/${upstreamURL.pathname}/${name}/blobs/${digest.digest}`)
+  url.protocol = upstreamURL.protocol
+  url.hostname = upstreamURL.hostname
+
+  const headers = new Headers()
+  headers.set('accept', '*/*')
+  headers.set('authorization', `Basic ${env.REGISTRY_TOKEN}`)
+
+  const res = await fetch(url, {headers})
+  if (!res.ok || !res.body) return res
+
+  await env.storage.put(`${name}/blobs/${digest.digest}`, res.body, {
+    sha256: digest.hash,
+    httpMetadata: {
+      contentEncoding: res.headers.get('content-encoding') ?? undefined,
+      contentType: res.headers.get('content-type') ?? undefined,
+    },
+  })
+
+  const obj = await env.storage.get(`${name}/blobs/${digest.digest}`)
+  if (!obj) return new Response(null, {status: 404})
+  return new Response(obj.body, res)
 }
 
-export const ERROR = {
-  BLOB_UNKNOWN: {
-    code: 'BLOB_UNKNOWN',
-    message: 'blob unknown to registry',
-  },
-  BLOB_UPLOAD_INVALID: {
-    code: 'BLOB_UPLOAD_INVALID',
-    message: 'blob upload invalid',
-  },
-  BLOB_UPLOAD_UNKNOWN: {
-    code: 'BLOB_UPLOAD_UNKNOWN',
-    message: 'blob upload unknown to registry',
-  },
-  DIGEST_INVALID: {
-    code: 'DIGEST_INVALID',
-    message: 'provided digest did not match uploaded content',
-  },
-  MANIFEST_BLOB_UNKNOWN: {
-    code: 'MANIFEST_BLOB_UNKNOWN',
-    message: 'manifest references a manifest or blob unknown to registry',
-  },
-  MANIFEST_INVALID: {
-    code: 'MANIFEST_INVALID',
-    message: 'manifest invalid',
-  },
-  MANIFEST_UNKNOWN: {
-    code: 'MANIFEST_UNKNOWN',
-    message: 'manifest unknown to registry',
-  },
-  NAME_INVALID: {
-    code: 'NAME_INVALID',
-    message: 'invalid repository name',
-  },
-  NAME_UNKNOWN: {
-    code: 'NAME_UNKNOWN',
-    message: 'repository name not known to registry',
-  },
-  SIZE_INVALID: {
-    code: 'SIZE_INVALID',
-    message: 'provided length did not match content length',
-  },
-  UNAUTHORIZED: {
-    code: 'UNAUTHORIZED',
-    message: 'authentication required',
-  },
-  DENIED: {
-    code: 'DENIED',
-    message: 'requested access to the resource is denied',
-  },
-  UNSUPPORTED: {
-    code: 'UNSUPPORTED',
-    message: 'the operation is unsupported',
-  },
-  TOOMANYREQUESTS: {
-    code: 'TOOMANYREQUESTS',
-    message: 'too many requests',
-  },
+export async function resolveTagToDigest(env: Env['Bindings'], name: string, tag: string) {
+  const obj = await env.storage.get(`${name}/tags/${tag}`)
+  if (obj) return parseDigest(await obj.text())
+
+  upstreamURL = upstreamURL ?? new URL(env.UPSTREAM_REGISTRY)
+
+  const url = new URL(`https://registry/v2/${upstreamURL.pathname}/${name}/manifests/${tag}`)
+  url.protocol = upstreamURL.protocol
+  url.hostname = upstreamURL.hostname
+
+  const headers = new Headers()
+  headers.set('accept', '*/*')
+  headers.set('authorization', `Basic ${env.REGISTRY_TOKEN}`)
+
+  const res = await fetch(url, {headers})
+  if (!res.ok) return null
+
+  const digest = res.headers.get('Docker-Content-Digest')
+  if (!digest) return null
+
+  await env.storage.put(`${name}/tags/${tag}`, digest)
+  return parseDigest(digest)
+}
+
+export async function getObject(req: HonoRequest<any>, env: Env['Bindings'], key: string, headers: Headers) {
+  const obj = await env.storage.get(key, {onlyIf: req.headers, range: req.headers})
+  if (!obj) return null
+
+  obj.writeHttpMetadata(headers)
+  headers.set('etag', obj.httpEtag)
+
+  if (
+    obj.range &&
+    'offset' in obj.range &&
+    'length' in obj.range &&
+    obj.range.offset != undefined &&
+    obj.range.length != undefined
+  ) {
+    headers.set('content-range', `bytes ${obj.range.offset}-${obj.range.offset + obj.range.length - 1}/${obj.size}`)
+    headers.set('content-length', (obj.range.length != undefined ? obj.range.length : obj.size).toString())
+  } else {
+    headers.set('content-length', obj.size.toString())
+  }
+
+  const body = 'body' in obj && obj.body ? obj.body : null
+  const status = 'body' in obj && obj.body ? (req.headers.get('range') !== null ? 206 : 200) : 304
+  console.log('Handling with R2', key, status, obj.range, JSON.stringify([...headers.entries()]))
+  return new Response(body, {headers, status})
+}
+
+const tagSchema = z.string().regex(/^[\w][\w.-]{0,127}$/)
+
+export function parseTag(candidate: string) {
+  const result = tagSchema.safeParse(candidate)
+  return result.success ? result.data : null
+}
+
+export interface Digest {
+  readonly algorithm: 'sha256' | 'sha384' | 'sha512'
+  readonly hash: string
+  readonly digest: string
+}
+
+const digestSchema = z.string().regex(/^[a-z0-9]+(?:[.+_-][a-z0-9]+)*:[a-zA-Z0-9=_-]+$/)
+export function parseDigest(candidate: string): Digest | null {
+  const result = digestSchema.safeParse(candidate)
+  if (!result.success) return null
+  const [algorithm, hash] = result.data.split(':')
+  if (algorithm !== 'sha256' && algorithm !== 'sha384' && algorithm !== 'sha512') return null
+  if (!hash) return null
+  return {algorithm, hash, digest: result.data}
 }
