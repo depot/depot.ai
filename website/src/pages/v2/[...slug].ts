50c8b9f39ff9bb3da90ff3530335ab911dd8d7a2
Jacob Gillespie <jacobwgillespie@gmail.com>
2023-07-08 09:32:57 +0100

    Remove unused files

--- website/src/pages/v2/[...slug].ts
+++
@@ -1,9 +0,0 @@
-import {getRuntime} from '@astrojs/cloudflare/runtime'
-import type {APIRoute} from 'astro'
-import {onRequest} from '../../registry'
-
-export const all: APIRoute = ({request}) => {
-  const runtime = getRuntime(request)
-  const eventContext = {...runtime, request} as unknown as EventContext<any, string, any>
-  return onRequest(eventContext)
-}
